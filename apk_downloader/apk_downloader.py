edc7d9718bc3469d00c6879d6a501ec00629a2ed
kibua20 <65806688+kibua20@users.noreply.github.com>
2020-08-04 16:27:23 +0900

    apk_downloader - https://kibua20.tistory.com/102

--- apk_downloader/apk_downloader.py
+++ apk_downloader/apk_downloader.py
@@ -4,6 +4,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+import os
 import time
 
 def main():
@@ -17,23 +18,27 @@ def main():
     apk_download (apklist) 
 
 def apk_download(apklist):
+    # download 와 profile path 지정
+    download_dir = os.path.join(os.getcwd(), 'download')
+    profile_dir = os.path.join(os.getcwd(), 'profile')
+
     option = Options()
     option.add_argument("disable-infobars")
     option.add_argument("disable-extensions")
     #option.add_argument("start-maximized")
     option.add_argument('disable-gpu')
-    # 백그라운드로 다운로드
-    option.add_argument('headless')
-    option.add_argument("user-data-dir=./profile")
+    #option.add_argument('headless')
 
-    # 안전하지 않은 콘텐츠 다운로드 팝업을 표시하지 않도록 옵션 설정
+    option.add_argument("user-data-dir="+profile_dir)
+
     option.add_experimental_option("prefs", {
-        "download.default_directory": r"./download",
+        "download.default_directory":  download_dir,
         "download.prompt_for_download": False,
         "download.directory_upgrade": True,
         "safebrowsing.enabled": True
     })
 
+    # webdriver 얻어옴
     browser = webdriver.Chrome(options=option)
 
     # apk list 에서 apk download 시도함 
@@ -43,8 +48,8 @@ def apk_download(apklist):
         browser.get(apk_mirror_url)
         time.sleep(10)
 
-    # chrome에서 다운로드 완료 할때까지 충분한 시간을 기다림
-    time.sleep(30*10)
+    # chrome에서 다운로드 완료 할때 까지 충분한 시간을 기다림
+    time.sleep(30)
 
     # chrome 종료
     browser.quit()
