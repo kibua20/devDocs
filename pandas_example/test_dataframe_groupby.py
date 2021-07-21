ef6e5d55061d78120c43d7068d474275367c323c
kibua20 <kibua20@gmail.com>
2021-07-22 00:40:04 +0900

    Add dataframe samples

--- pandas_example/test_dataframe_groupby.py
+++ pandas_example/test_dataframe_groupby.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-
 import pandas as pd
 
+def filter_func(df, x):
+    #print (x['Test1'].mean)
+    return df['Test1'] < x
+
 # csv file에서 읽기
-df = pd.read_csv('./Grade_sample.csv')
-print (df)
+#df = pd.read_csv('./Grade_sample.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/kibua20/devDocs/master/pandas_example/Grade_Sample.csv')
+#print (df)
+
+# groupby()
 df_groupby = df.groupby(['Grade']).mean()
-print (df_groupby)
+#print (type(df_groupby))
+#print ('GropyBy Result:')
+#print (df_groupby)
+
+
+# df_filter = df.groupby(['Grade'])
+# #print (df_filter)
+# df_filter = df.groupby(['Grade']).filter(lambda x: x['Grade'].mean() > 3)
+# print (df_filter)
